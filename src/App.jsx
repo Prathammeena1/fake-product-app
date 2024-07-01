6617d35b601c7c9a4731dfa8351fdf57d31a9dc1
pratham meena <yashmeena274@gmail.com>
2024-07-01 23:46:30 +0530

    nav add product button created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ProductCard from './components/ProductCard';
 import Header from './components/Header';
+import Nav from './Nav';
 
 const products = [
   { name: 'Nike', model: 'Roshe One Essential ID', price: 110, designer: 'Unknown', image: '/images/nike.png' },
@@ -14,6 +15,9 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-zinc-900 to-zinc-950 p-5 text-zinc-200">
       <Header/>
+      <div className='flex mt-5'>
+        <Nav/>
+      </div>
       
     </div>
   );
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='h-[65vh] w-[20vw] bg-gradient-to-tl from-zinc-800 to-zinc-700 rounded-lg text-center'>
+        <button className='bg-gradient-to-br from-zinc-400 to-zinc-100 text-zinc-800 font-[600] text-xl rounded-md px-4 py-1 mt-4'>Add new product</button>
+    </div>
+  )
+}
+
+export default Nav
