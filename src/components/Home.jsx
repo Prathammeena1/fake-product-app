b6560a8f7ec1b137eb6747bc14cb2c1de9fd58b1
pratham meena <yashmeena274@gmail.com>
2024-07-02 16:21:35 +0530

    axios setup done !

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard';
+import Axios from './axios/Axios';
 
 
 const Home = () => {
-  const products = [
-    { name: 'Nike', model: 'Roshe One Essential ID', price: 110, designer: 'Unknown', image: '/images/nike.png' },
-    { name: 'Adidas', model: 'NMD City Sock 2', price: 130, designer: 'Kith Unveil', isNew: true, image: '/images/adidas.png' },
-    { name: 'Reebok', model: 'Classic Nylon Magic', price: 90, originalPrice: 110, designer: 'Unknown', image: '/images/reebok.png' },
-    // Add more products as needed
-  ];
+  const [products, setproducts] = useState([])
+
+  const getData = async () =>{
+    const response = await Axios.get('/products?limit=5')
+    setproducts(response.data)
+  }
+  
+  useEffect(() => {
+    getData()
+  }, [products])
+  
+  console.log(products)
   return (
     <div className='h-[65vh] w-[78vw] bg-gradient-to-tl from-zinc-600 to-zinc-800 rounded-lg flex gap-4 p-4'>
       {products.map((product,i) => <ProductCard key={i} product={product}  />)}
