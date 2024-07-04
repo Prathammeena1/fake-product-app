f3048080af1b383571d1f13591a55e8ea065a0a5
pratham meena <yashmeena274@gmail.com>
2024-07-04 12:02:22 +0530

    ui developed for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   const [products, setproducts] = useState([])
 
   const getData = async () =>{
-    const response = await Axios.get('/products?limit=5')
+    const response = await Axios.get('/products?limit=10')
     setproducts(response.data)
   }
   
@@ -17,7 +17,7 @@ const Home = () => {
   
   console.log(products)
   return (
-    <div className='h-[65vh] w-[78vw] bg-gradient-to-tl from-zinc-600 to-zinc-800 rounded-lg flex gap-4 p-4'>
+    <div className=' main h-[65vh] w-[78vw] bg-gradient-to-tl from-zinc-600 to-zinc-800 rounded-lg p-4 flex gap-4 flex-wrap overflow-y-auto'>
       {products.map((product,i) => <ProductCard key={i} product={product}  />)}
     </div>
   )
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="bg-white p-4 shadow-md w-[30%] h-fit bg-gradient-to-tl from-zinc-900 to-zinc-800 rounded-lg">
-      <div className="relative">
-        <img src={product.image} alt={product.title} className="w-full h-48 object-cover rounded-t-lg" />
-        {product.isNew && <span className="absolute top-0 right-0 bg-red-500 text-white text-xs px-2 py-1">NEW</span>}
+    <div className="bg-white p-4 min-h-[320px] shadow-md w-[23.5%] h-fit bg-gradient-to-tl from-zinc-900 to-zinc-800 rounded-lg flex-shrink-0">
+      <div className="h-48 w-[85%] overflow-hidden rounded-lg text-center mx-auto">
+        <img src={product.image} alt={product.title} className="w-full h-full object-cover" />
       </div>
       <div className="mt-4">
-        <h3 className="text-lg font-bold">{product.title}</h3>
+        <h3 className="text-sm font-bold">{product.title.split(" ").slice(0,3).join(" ") + "..."}</h3>
         <span className="text-gray-100 text-xl font-semibold">${product.price}</span>
-        <p className="text-gray-500 text-sm">{product.description}</p>
+        <p className="text-gray-500 text-xs">{product.description.split(" ").slice(0,7).join(" ") + "..."}</p>
       </div>
     </div>
   );
