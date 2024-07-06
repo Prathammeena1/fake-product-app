3f4e66ced091bd0ed2a92a5ad60e22f0086301f6
pratham meena <yashmeena274@gmail.com>
2024-07-06 12:46:40 +0530

    header part done!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+
+  const {search} = useLocation()
+  const [category, setcategory] = useState("")
+  
+  useEffect(() =>{
+    setcategory("")
+    if(search.trim() != ''){
+      setcategory(decodeURIComponent(search.split('=')[1]))
+    }
+  },[search])
+
   return (
     <header className="bg-gradient-to-tr from-zinc-800 to-zinc-900 shadow-md py-6 px-4 sm:px-6 lg:px-8 rounded-lg">
       <div className="mx-auto  flex justify-between items-center">
@@ -11,7 +23,7 @@ const Header = () => {
         </div>
       </div>
       <div className="flex w-[90vw] justify-between items-center py-2 mt-4">
-        <div className="capitalize text-xl font-bold text-zinc-600">Home <span className="text-zinc-300"><i className="ri-arrow-drop-right-line"></i>sneakers </span></div>
+        <div className="capitalize text-xl font-bold text-zinc-600"><Link to='/'>Home</Link> {category.trim() != "" && <span className="text-zinc-300"><i className="ri-arrow-drop-right-line"></i>{category} </span> }</div>
         <div className=" w-[60%]">
         <input type="text" placeholder="Search..." className="bg-transparent w-full focus:outline-none focus:border-none placeholder:text-zinc-600 placeholder:font-bold text-6xl text-zinc-300 font-medium" />
         </div>
