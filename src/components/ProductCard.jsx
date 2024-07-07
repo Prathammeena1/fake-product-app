9b05b1ea0bb472d92f69192ebf38ff1bfddd192b
pratham meena <yashmeena274@gmail.com>
2024-07-07 14:58:03 +0530

    edited button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { setData } from './store/slice/productsSlice';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { setData } from "./store/slice/productsSlice";
+import { useDispatch } from "react-redux";
 
-const ProductCard = ({ product,products }) => {
-
-  const dispatch = useDispatch()
-  const deleteHandler = ()=>{
-    const filteredProducts = products.filter(p=> p.id !== product.id)
-    dispatch(setData(filteredProducts))
-  }
+const ProductCard = ({ product, products }) => {
+  const dispatch = useDispatch();
+  const deleteHandler = () => {
+    const filteredProducts = products.filter((p) => p.id !== product.id);
+    dispatch(setData(filteredProducts));
+  };
 
   return (
     <div className="bg-white p-4 min-h-[380px] h-fit shadow-md w-[23.5%] cursor-pointer bg-gradient-to-tl from-zinc-900 to-zinc-800 rounded-lg flex-shrink-0">
       <div className="h-48 w-[85%] overflow-hidden rounded-lg text-center mx-auto">
-        <img src={product.image} alt={product.title} className="w-full h-full object-cover" />
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full h-full object-cover"
+        />
       </div>
       <div className="mt-4">
-        <h3 className="text-sm font-bold">{product.title.split(" ").slice(0,3).join(" ") + "..."}</h3>
-        <span className="text-gray-100 text-xl font-semibold">${product.price}</span>
-        <p className="text-gray-500 text-xs">{product.description.split(" ").slice(0,3).join(" ") + "..."}</p>
+        <h3 className="text-sm font-bold">
+          {product.title.split(" ").slice(0, 3).join(" ") + "..."}
+        </h3>
+        <span className="text-gray-100 text-xl font-semibold">
+          ${product.price}
+        </span>
+        <p className="text-gray-500 text-xs">
+          {product.description.split(" ").slice(0, 3).join(" ") + "..."}
+        </p>
       </div>
-      <div className='flex gap-1 justify-between mt-4'>
-      <Link to={`/edit/${product.id}`} className='bg-gradient-to-br from-blue-500 to-blue-950 hover:bg-gradient-to-br hover:from-blue-950 hover:to-blue-500 transition-all ease-in-out text-zinc-200 font-[600] text-xl rounded-md px-7 py-1'>Edit</Link>
-      <button onClick={deleteHandler} className='bg-gradient-to-br from-red-950 to-red-500 hover:bg-gradient-to-br hover:from-red-500 hover:to-red-950 transition-all ease-in-out text-zinc-200 font-[600] text-xl rounded-md px-7 py-1'>Delete</button>
-
+      <div className="flex gap-1 justify-between mt-4">
+        <Link
+          to={`/edit/${product.id}`}
+          className=""
+        >
+          <button className="bg-zinc-800 text-zinc-100 border border-zinc-700 border-b-4 font-medium overflow-hidden relative px-8 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+  <span className="bg-zinc-200 shadow-zinc-200 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+          Edit
+</button>
+        </Link>
+        <button onClick={deleteHandler} className="bg-zinc-800 text-zinc-100 border border-zinc-700 border-b-4 font-medium overflow-hidden relative px-8 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+  <span className="bg-zinc-200 shadow-zinc-200 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+  Delete
+</button>
       </div>
     </div>
   );
