2522dcc1765966b60acf57648fdbc5e562f101c4
pratham meena <yashmeena274@gmail.com>
2024-07-15 19:20:55 +0530

    .

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,7 +35,7 @@ const Nav = () => {
         <Link to='/create' className='bg-gradient-to-br from-zinc-400 to-zinc-100 hover:bg-gradient-to-br hover:from-zinc-100 hover:to-zinc-400 transition-all ease-in-out text-zinc-800 font-[600] text-xl rounded-md px-4 py-1'>Add new product</Link>
         <div key={1} className='flex flex-col w-fit ml-10 mt-5'>
             <h1 className='text-xl mb-3'>Category</h1>
-            {categories ? categories.map((category,i) => <div key={i} className="cat1 flex items-center gap-3 ml-2 text-sm">
+            {categories.length > 0 ? categories.map((category,i) => <div key={i} className="cat1 flex items-center gap-3 ml-2 text-sm">
                 <div className="square h-3 w-3 bg-zinc-400 rounded-sm"></div>
                 <Link to={`/?category=${category}`}  className='w-fit capitalize'>{category}</Link>
             </div> ) : <div>No categories </div> }
